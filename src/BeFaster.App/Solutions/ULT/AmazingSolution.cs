75d6aa27565efc78a9187bfeceb59b56c2a82b60
sbx_user1051 <sbx_user1051@169.254.10.97>
2025-07-03 17:10:14 +0000

    Thu Jul 03 17:10:14 UTC 2025

--- src/BeFaster.App/Solutions/ULT/AmazingSolution.cs
+++ src/BeFaster.App/Solutions/ULT/AmazingSolution.cs
@@ -1,4 +1,5 @@
 using BeFaster.Runner.Exceptions;
+using System.Diagnostics;
 
 namespace BeFaster.App.Solutions.ULT
 {
@@ -6,7 +7,32 @@ namespace BeFaster.App.Solutions.ULT
     {
         public string UltimateMaze(int rows, int columns, Dictionary<string, string> mazeGenerationOptions)
         {
-            throw new SolutionNotImplementedException();
+            //throw new SolutionNotImplementedException();
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "Legacy.exe", // Or python, amazing.sh, etc.
+                    Arguments = columns.ToString() + " " + rows.ToString(),
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+
+            //using (StreamWriter writer = process.StandardInput)
+            //{
+            //    writer.WriteLine(columns); // Width
+            //    writer.WriteLine(rows);    // Length
+            //}
+
+            string output = process.StandardOutput.ReadToEnd().Trim();
+            process.WaitForExit();
+            return output;
         }
     }
 }
+
